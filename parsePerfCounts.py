600dbdde3363631f7d9ea5d6b49c545d2dfcbbbe
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2024-02-28 15:22:09 +0100

    produce wiki page

---
+++ parsePerfCounts.py
@@ -0,0 +1,45 @@
+def parseOne(fname) :
+  nop=0
+  d ={}
+  with open(fname) as f:
+    print('\n\n---++ '+fname)
+    for line in f:
+      try:
+        (wf, val, key) = line.split()
+        d[wf] = {}
+      except:
+        nop+=1
+  with open(fname) as f:
+    for line in f:
+      try:
+        (wf, val, key) = line.split()
+        d[wf][key]= float(val)*1.e-9
+        if (key=='task-clock') : d[wf][key] = float(val)
+       	if (key=='duration') : d[wf][key] = float(val)
+      except:
+        nop+=1
+#
+  do = {}
+  for wf in d :
+    for k in d[wf] :
+      ko = k
+      if not ko in do : do[ko] = {}
+      do[ko][wf] = d[wf][k]
+  return do
+
+def doPrint(di) :
+  s = '|' + ' | '
+  for wf  in di['cycles'] :
+    s+= ' ' + wf + ' |'
+  s += '|'
+  print(s)
+  for k in di :
+    s = '|' + k + ' | '
+    for wf  in di[k] :
+        v = di[k][wf]
+        s+= ' ' + "{:6.4f}".format(v)  + ' | '
+    s += '|'
+    print(s)
+
+
+doPrint(parseOne('data/PerfGenoaLU'))
